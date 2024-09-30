4295275fadd8b03dcc050987e8ba8ec0b5eaf750
Ramsnes <115258511+Ramsnes@users.noreply.github.com>
2024-09-30 21:34:34 +0200

    tailwind not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <section className="grid gap-8">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img
+          src="https://akamai.vgc.no/users/schibsted/images/114785677.jpg?t[strip]=1&t[crop][x]=0&t[crop][y]=0&t[crop][width]=720&t[crop][height]=529&t[resize][width]=720&accessToken=4718d5ae8a04312439821a6d40875ac1ec95d4a768a863281e98870c046207b5"
+          alt="#"
+        ></img>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div>
+        <h1 className="text-4xl font-medium mt-4">Heading</h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
+          accusamus nemo qui doloremque dolorum esse eligendi eum harum rerum
+          exercitationem?
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </section>
+  );
 }
 
-export default App
+export default App;
