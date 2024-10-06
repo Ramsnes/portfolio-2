cf09f199237cf2984d259a8ae2fc3e4a5295f626
Ramsnes <115258511+Ramsnes@users.noreply.github.com>
2024-10-06 17:38:13 +0200

    fixed img size and position

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,7 +4,7 @@ import mortenRamfjord from "../assets/pfb-cropped.jpg";
 export const Intro = () => {
   return (
     <div className="grid w-full gap-5 md:grid-cols-2">
-      <div className="intro-container group flex flex-col justify-start md:text-left md: md:h-5/6 lg:h-5/6 order-2 md:order-1 gap-5">
+      <div className="intro-container group flex flex-col justify-start md:text-left md:h-5/6 lg:h-5/6 order-2 md:order-1 gap-5">
         <h2 className="text-2xl p-5">About me</h2>
         <p className="intro-paragraph fadeInL">
           I’m a front-end developer passionate about building dynamic and
@@ -15,11 +15,13 @@ export const Intro = () => {
           to embrace new challenges and opportunities to grow as a developer.
         </p>
       </div>
-      <img
-        className="fadeInR w-full col-span-1 md:rounded-s order-1 md:order-2"
-        src={mortenRamfjord}
-        alt="Morten Ramfjord profile picture"
-      />
+      <div className="flex justify-center items-center order-1 md:order-2">
+        <img
+          className="fadeInR w-full md:w-2/3 md:rounded-s"
+          src={mortenRamfjord}
+          alt="Morten Ramfjord profile picture"
+        />
+      </div>
     </div>
   );
 };
